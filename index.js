e534bb8acca7e350823637d44bae4e4142480025
kmTyagi198 <k.m.tyagi198@gmail.com>
2026-01-13 01:07:33 +0530

    added creating task with enter key

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ var inProgressDiv = document.getElementById("inProgressDiv");
 var inReviewDiv = document.getElementById("inReviewDiv");
 var inDoneDiv = document.getElementById("inDoneDiv");
 
+
+document.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        const newTaskInput = document.getElementById("newTask");
+        const newTask = newTaskInput.value;
+        if (newTask === '') return;
+        newTaskInput.value = "";
+        var newTaskDiv = document.createElement('div');
+        newTaskDiv.classList.add('taskTemp');
+        newTaskDiv.setAttribute('data-status', 'toDo');
+        newTaskDiv.innerHTML = `<p class='task'>` + newTask + `</p><div>
+                    <div data-action="delete" class="actionBtn"">Del</div>
+                    <div data-action="move" class="actionBtn">move</div> </div>`;
+        document.getElementById("toDoDiv").appendChild(newTaskDiv);
+        return;
+
+    }
+})
+
 document.addEventListener('click', (e) => {
 
     const btn = e.target.closest('.actionBtn');
@@ -9,7 +28,7 @@ document.addEventListener('click', (e) => {
     if (btn.dataset.action === 'create') {
         const newTaskInput = document.getElementById("newTask");
         const newTask = newTaskInput.value;
-        if(newTask === null) return;
+        if (newTask === '') return;
         newTaskInput.value = "";
         var newTaskDiv = document.createElement('div');
         newTaskDiv.classList.add('taskTemp');
